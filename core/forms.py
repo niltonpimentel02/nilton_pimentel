7ea1722ff4e9fb72d794f75001ee01b4b6e5c208
Nilton Pimentel <niltonpimentel02@gmail.com>
2021-07-09 11:28:56 -0300

    changed form contact error messages

--- core/forms.py
+++ core/forms.py
@@ -3,15 +3,16 @@ from django.core.mail.message import EmailMessage
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='', max_length=100, widget=forms.TextInput(attrs={'class':'form-control',
-                                                                                   'placeholder':'Nome'}))
-    email = forms.EmailField(label='', max_length=100, widget=forms.EmailInput(attrs={'class':'form-control',
-                                                                                      'placeholder':'Email'}),)
-    phone = forms.CharField(label='', max_length=20, widget=forms.TextInput(attrs={'class':'form-control',
-                                                                    'placeholder': 'Celular'}))
-    subject = forms.CharField(label='', max_length=100, widget=forms.TextInput(attrs={'class':'form-control',
-                                                                                      'placeholder':'Assunto'}))
-    message = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'form-control', 'placeholder':'Mensagem'}))
+    name = forms.CharField(label='', max_length=100, error_messages={'required': 'Por favor, digite seu nome:'},
+                           widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome'}))
+    email = forms.EmailField(label='', max_length=100, error_messages={'required': 'Por favor, digite seu email:'},
+                             widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    phone = forms.CharField(label='', max_length=20, error_messages={'required': 'Por favor, digite seu celular:'},
+                            widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Celular'}))
+    subject = forms.CharField(label='', max_length=100, error_messages={'required': 'Por favor, digite um assunto:'},
+                              widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Assunto'}))
+    message = forms.CharField(label='', error_messages={'required': 'Por favor, digite sua mensagem:'},
+                              widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Mensagem'}))
 
     def send_mail(self):
         name = self.cleaned_data['name']
