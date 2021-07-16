fc0db627f80ccca51811b94d5e71f503de43d0da
Nilton Pimentel <niltonpimentel02@gmail.com>
2021-07-16 15:20:42 -0300

    created favicon close #10

--- nilton_pimentel/settings.py
+++ nilton_pimentel/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
