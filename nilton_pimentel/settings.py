e683faa5dc1a9071c6ba957f2b15b732f49a24ee
Nilton Pimentel <niltonpimentel02@gmail.com>
2021-07-16 15:50:33 -0300

    created favicon close #10

--- nilton_pimentel/settings.py
+++ nilton_pimentel/settings.py
@@ -123,8 +123,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-WHITENOISE_MANIFEST_STRICT = False
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
