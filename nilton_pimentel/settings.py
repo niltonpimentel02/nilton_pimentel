8021a6bd5892c7afd01c14390aeb57d52d3fd165
Nilton Pimentel <niltonpimentel02@gmail.com>
2022-10-22 14:32:50 -0300

    updated location

--- nilton_pimentel/settings.py
+++ nilton_pimentel/settings.py
@@ -152,4 +152,3 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-## laranjada
