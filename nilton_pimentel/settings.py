6a5a89f2e6b79e204c6f3c461168284812632637
Nilton Pimentel <niltonpimentel02@gmail.com>
2022-10-22 23:12:45 -0300

    updated build migrate

--- nilton_pimentel/settings.py
+++ nilton_pimentel/settings.py
@@ -134,7 +134,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 WHITENOISE_AUTOREFRESH=True
 
