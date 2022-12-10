3fa5f5c3ed25e8d0944a01b11f82d2c73a604ad5
Nilton Pimentel <niltonpimentel02@gmail.com>
2022-12-09 21:06:24 -0300

    code refactor 8

--- nilton_pimentel/settings.py
+++ nilton_pimentel/settings.py
@@ -154,3 +154,36 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+# if DEBUG:
+#     LOGGING = {
+#         "version": 1,
+#         "disable_existing_loggers": False,
+#         "formatters": {
+#             "verbose": {"format": "%(levelname)s %(asctime)s %(module)s " "%(process)d %(thread)d %(message)s"}
+#         },
+#         "handlers": {
+#             "console": {
+#                 "level": "DEBUG",
+#                 "class": "logging.StreamHandler",
+#                 "formatter": "verbose",
+#             }
+#         },
+#         "root": {"level": "INFO", "handlers": ["console"]},
+#     }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {"format": "%(levelname)s %(asctime)s %(module)s " "%(process)d %(thread)d %(message)s"}
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
